d594a2a5d24b2fc4390e285a9394c98e790f8be0
AlvaroGaribay1 <garciagaribayalvaro@gmail.com>
2025-03-02 00:22:54 -0600

    Contact page and footer implementation
    Temporal contact form and footer with temporal information

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,36 @@
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
+  animations: [
+        trigger('slideIn', [
+          state(
+            'void',
+            style({
+              opacity: 0,
+              transform: 'translateX(-50px)',
+            })
+          ),
+          transition(':enter', [
+            animate(
+              '0.7s ease-out',
+              keyframes([
+                style({ opacity: 0, transform: 'translateX(-50px)', offset: 0 }),
+                style({
+                  opacity: 0.5,
+                  transform: 'translateX(-20px)',
+                  offset: 0.5,
+                }),
+                style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
+              ])
+            ),
+          ]),
+        ]),
+      ]
 })
 export class AboutComponent {
 
