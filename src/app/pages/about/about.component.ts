1674dfed07369c6199ff6960d42c3230faafb90c
AlvaroGaribay1 <garciagaribayalvaro@gmail.com>
2025-03-02 20:51:50 -0600

    MailSender implementation , footer and navbar changes
    Added personalization to the underline navbar class and the capacity to send email through the contact form.

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,7 @@
 import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+ import emailjs from "emailjs-com";
+ import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-about',
@@ -7,31 +9,66 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
   animations: [
-        trigger('slideIn', [
-          state(
-            'void',
+    trigger('slideIn', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+          transform: 'translateX(-50px)',
+        })
+      ),
+      transition(':enter', [
+        animate(
+          '0.7s ease-out',
+          keyframes([
+            style({ opacity: 0, transform: 'translateX(-50px)', offset: 0 }),
             style({
-              opacity: 0,
-              transform: 'translateX(-50px)',
-            })
-          ),
-          transition(':enter', [
-            animate(
-              '0.7s ease-out',
-              keyframes([
-                style({ opacity: 0, transform: 'translateX(-50px)', offset: 0 }),
-                style({
-                  opacity: 0.5,
-                  transform: 'translateX(-20px)',
-                  offset: 0.5,
-                }),
-                style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
-              ])
-            ),
-          ]),
-        ]),
-      ]
+              opacity: 0.5,
+              transform: 'translateX(-20px)',
+              offset: 0.5,
+            }),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  ngOnInit(): void {
+    const form = document.getElementById('contact-form') as HTMLFormElement;
+
+    if (form) {
+      form.addEventListener('submit', (event: Event) => {
+        event.preventDefault();
+
+        emailjs
+          .sendForm(
+            'service_z9qome8',
+            'template_m9pqcs7',
+            form,
+            'U2cWyzOhBkOzX0PdJ'
+          )
+          .then(() => {
+            Swal.fire({
+              title: 'Done!',
+              text: 'Message sent!',
+              icon: 'success',
+              timer: 1500,
+            });
+            form.reset();
+          })
+          .catch((error) =>
+            Swal.fire({
+              title: 'Bad news!',
+              text: 'Message not sent!',
+              icon: 'error',
+              timer: 1500,
+            })
+          );
+      });
+    }
+  }
+
 
 }
